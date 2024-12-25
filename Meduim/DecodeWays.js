c2da0ffedfc3ab74d203491250722875cb93ce4c
ebrahimam <ebrahim.ros78@gmail.com>
2024-12-25 20:11:07 +0330

    Decode Ways

---
+++ Meduim/DecodeWays.js
@@ -0,0 +1,13 @@
+// MEDIUM
+// STRIGN
+
+// Problem
+
+// A message containing letters from A-Z is being encoded to numbers using the following mapping:
+// 'A' -> 1
+// 'B' -> 2
+// ...
+// 'Z' -> 26
+// Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+
