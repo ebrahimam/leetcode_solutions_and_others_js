b41ed6c6d8e353fa9a33be7d20af9372fa9e5365
ebrahimam <ebrahim.ros78@gmail.com>
2025-01-02 22:11:40 +0330

    Container With Most Water

---
+++ Meduim/ContainerWithMostWater.js
@@ -0,0 +1,32 @@
+// Meduim
+// Array
+
+// Problem
+// Given n non-negative integers a1, a2, …, an, where each represents a point at coordinate (i, ai).
+// n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, 
+// which together with x-axis forms a container, such that the container contains the most water.
+// Note: You may not slant the container and n is at least 2.
+
+
+var maxArea = function(height) {
+    var max = 0;
+    var l = 0;
+    var r = height.length - 1;
+    while (l < r) {
+      max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+      if (height[l] < height[r]) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+    return max;
+  };
+
+  
+//   Complexity:
+
+//     Time complexity : O(n).
+//     Space complexity : O(1).
+
+
